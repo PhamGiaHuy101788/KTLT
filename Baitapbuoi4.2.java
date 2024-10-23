1213dde8a7ec8ce06d5d192ef73b323dfa4af378
PhamGiaHuy101788 <150434312+PhamGiaHuy101788@users.noreply.github.com>
2024-10-23 16:12:03 +0700

    Baitapbuoi4.2.java

---
+++ Baitapbuoi4.2.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Khai báo và khởi tạo mảng
+        String[] nguHanh = {"kim", "mộc", "thủy", "hỏa", "thổ"};
+        
+        // Hiển thị các phần tử của mảng
+        System.out.println("Các phần tử của mảng Ngũ Hành là:");
+        for (int i = 0; i < nguHanh.length; i++) {
+            System.out.println(nguHanh[i]);
+        }
+    }
+}
