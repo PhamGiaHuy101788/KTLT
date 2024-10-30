1436e61c239b7d937fb7005583b9a7b0e5b39af6
PhamGiaHuy101788 <150434312+PhamGiaHuy101788@users.noreply.github.com>
2024-10-30 15:55:17 +0700

    BaiTapbuoi2.java

---
+++ B. THỰC HÀNH/TUẦN 2/BaiTapbuoi2.java
@@ -0,0 +1,16 @@
+package buoi2;
+
+public class An12345 {
+    public static void main(String[] args) {
+        // In ra lời chào tân sinh viên
+        String tenLop = "KTPM01";  // Bạn có thể thay thế tên lớp bằng tên lớp của bạn
+        System.out.println("Xin chào các bạn tân sinh viên lớp " + tenLop);
+        System.out.println("Xin chào các bạn tân sinh viên lớp \"" + tenLop + "\"");
+        
+        // In ra giá trị của cửa số
+        int cuaSo1 = 10;
+        double cuaSo2 = 5.5;
+        System.out.println("Giá trị cửa số1 là " + cuaSo1);
+        System.out.println("Giá trị cửa số2 là " + cuaSo2);
+    }
+}
